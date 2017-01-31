2ccda8467dedd35f509a3300fc11bfb2ca51b489
SirWernich <melktert@gmail.com>
2017-01-31 12:33:46 +0200

    first lecture's idle commands

---
+++ 01_numbers/01_basic_arithmetic.py
@@ -0,0 +1,25 @@
+Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:19:22) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 3/2
+1
+>>> float(3) / 2
+1.5
+>>> from __future__ import division
+>>> 3.2
+3.2
+>>> 3/2
+1.5
+>>> 
+=============================== RESTART: Shell ===============================
+>>> a = 5
+>>> a
+5
+>>> a += 3
+>>> a
+8
+>>> my_income = 100
+>>> tax_rate = 0.1
+>>> my_taxes = my_income * tax_rate
+>>> my_taxes
+10.0
+>>> 
